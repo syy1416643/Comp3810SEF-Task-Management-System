836b81378af1c6f303e0a89fa6a40db7539bffd5
syy1416653 <s1416643@live.hkmu.edu.hk>
2025-12-02 18:23:37 +0800

    fix

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.use(session({
 	ttl: 24*60*60
     }),
     cookie: {
-        secure: process.env.NODE_ENV === 'production',
-	sameSite: process.env.NODE_ENV === 'production' ? 'none' :'lax',
+        secure: true,
+	sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true
     }
